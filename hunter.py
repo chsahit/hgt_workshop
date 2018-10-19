96976aa007a4509e324e47c82c75dcdddc1fcd2d
Sahit Chintalapudi <chsahit@gmail.com>
2018-10-19 17:43:18 -0400

    update hunter with better refresh

--- hunter.py
+++ hunter.py
@@ -50,13 +50,15 @@ while(np.size(at_pose(x, goal_points, rotation_error=5)) != N):
     r.step()
 start_time = time.time()
 robot1_vel = np.random.uniform(-0.1, 0.1, size=(3, 1))
-while time.time() - start_time < 15:
+iterations = 0
+while time.time() - start_time < 20:
 
     # Get poses of agents
     x = r.get_poses()
     x_si = x[:2, :]
-    if (time.time() % 2 < 0.1):
+    if (iterations % 200 == 0):
         robot1_vel = np.random.uniform(-0.1, 0.1, size=(3, 1))
+        robot1_vel[2, 0] = 0.0
     robot2_vel = 0.3 * np.array([[x[0, 0] - x[0, 1]], [x[1, 0] - x[1, 1]], [0]])
     dxi = np.concatenate((robot1_vel, robot2_vel), axis=1)
     #print(dxi)
@@ -64,6 +66,7 @@ while time.time() - start_time < 15:
     r.set_velocities(np.arange(N), single_integrator_to_unicycle2(dxi, x))
     time.sleep(0.01)
     # Iterate the simulation
+    iterations += 1
     r.step()
 
 # Always call this function at the end of your scripts!  It will accelerate the
